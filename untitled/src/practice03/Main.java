d87038fd06ce1868b8034b37fa12d33704a82046
tobe1205@hanmail.net <tobe1205@hanmail.net>
2022-01-14 02:56:59 +0900

    [22.01.13] # CodeUp 기초 100제 - 1020 ~ 1023번 문제 풀이

---
+++ untitled/src/practice03/Main.java
@@ -0,0 +1,38 @@
+package practice03;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        //1020번 주민번호 입력받아 형태 바꿔 출력하기기
+        /*Stringn = sc.next();
+        n = n.replace("-","");
+        System.out.println(n);*/
+
+        //1021번 단어 1개 입력받아 그대로 출력하기
+       /* String n = sc.next();
+        System.out.println(n);*/
+
+        //1022번 문장 1개 입력받아 그대로 출력하기
+        /*String n = sc.nextLine();
+        System.out.println(n);*/
+
+        //1023번 실수 1개 입력받아 부분별로 출력하기
+
+        /*String s = sc.nextLine();
+        String[] arr = s.split("\\.");
+        int A = Integer.parseInt(arr[0]);
+        int B = Integer.parseInt(arr[1]);
+        System.out.printf("%d\n%d",A,B);*/
+
+
+
+
+        }
+    }
