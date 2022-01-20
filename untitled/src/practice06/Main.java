d007d40a610e50dbe3ada69704b011dfbfa72fde
tobe1205@hanmail.net <tobe1205@hanmail.net>
2022-01-20 20:00:28 +0900

    [22.01.20] # CodeUp 기초 100제 - 1049 ~ 1055번 문제 풀이

---
+++ untitled/src/practice06/Main.java
@@ -0,0 +1,97 @@
+package practice06;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+       /* //1049번문제
+        Scanner sc = new Scanner(System.in);
+        String[] str = sc.nextLine().split(" ");
+        int A = Integer.parseInt(str[0]);
+        int B = Integer.parseInt(str[1]);
+        int result;
+        if (A > B) {
+            result = 1;
+        } else {
+            result = 0;
+        }
+        System.out.println(result);*/
+//============================
+
+        //1050번문제
+        /*Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a == b)
+            System.out.print("1");
+        else
+            System.out.print("0");*/
+//============================
+
+        /*//1051번 문제
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (b >= a)
+            System.out.print("1");
+        else
+            System.out.print("0");*/
+
+        /*//============================
+
+        //1052번 문제
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (b != a)
+            System.out.print("1");
+        else
+            System.out.print("0");*/
+
+       /* //============================
+
+        //1053번 문제
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if(a != 0)
+            System.out.print(0);
+        else
+            System.out.print(1);*/
+
+        //============================
+
+        /*//1054번 문제
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if( a == 1 && b == 1 )
+            System.out.print(1);
+        else
+            System.out.print(0);*/
+
+        //============================
+
+        //1055번 문제
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if( a == 1 || b == 1 )
+            System.out.print(1);
+        else
+            System.out.print(0);
+
+
+
+
+
+
+    }
+}
+
