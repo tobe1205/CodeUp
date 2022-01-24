a8bbba415207299f45179e1357c58819a413c973
tobe1205@hanmail.net <tobe1205@hanmail.net>
2022-01-24 21:04:57 +0900

    [22.01.24] # CodeUp 기초 100제 - 1056 ~ 1060번 문제 풀이

---
+++ untitled/src/practice07/Main.java
@@ -0,0 +1,63 @@
+package practice07;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*//1056번문제
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a!= b){
+            System.out.println(1);
+        } else System.out.println(0);*/
+        //=====================================
+
+        /*//1057번문제
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a == b) {
+            System.out.println(1);
+        } else System.out.println(0);*/
+        //=====================================
+
+        /*//1058번문제
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a == 0 && b == 0) {
+            System.out.println(1);
+        } else System.out.println(0);
+        //=====================================*/
+
+        /*//1059번문제
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        System.out.println(~a);*/
+
+        //=====================================
+
+        //1060번문제
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a&b);
+
+        //=====================================
+
+
+    }
+}
