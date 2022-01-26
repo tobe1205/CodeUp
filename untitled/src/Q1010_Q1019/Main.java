6da575b0a4eb690252582a1e9045e9a560e3a49d
tobe1205@hanmail.net <tobe1205@hanmail.net>
2022-01-26 17:33:44 +0900

    [22.01.14] # CodeUp 기초 100제 - 1010 ~ 1019번 문제 풀이

--- untitled/src/Q1010_Q1019/Main.java
+++ untitled/src/Q1010_Q1019/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+
         // 1010번 : 정수 1개 입력받아 그대로 출력하기
         /*int n;
         n = sc.nextInt();
