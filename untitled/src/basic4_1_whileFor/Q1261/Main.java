5b39d3b5608c1cf052357ab1ee7cfe79de13de59
tobe1205@hanmail.net <tobe1205@hanmail.net>
2022-02-17 22:01:09 +0900

    [22.02.17] # CodeUp 1261번 문제 풀이

--- untitled/src/basic4_1_whileFor/Q1261/Main.java
+++ untitled/src/basic4_1_whileFor/Q1261/Main.java
@@ -1,19 +1,21 @@
 package basic4_1_whileFor.Q1261;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] a = sc.nextLine().split(" ");
+        int b = 0;
         for (int i = 0; i < 10; i++) {
-
-
+            b = Integer.parseInt(a[i]);
+            if (b % 5 == 0) {
+                System.out.println(b);
+                break;
+            }
         }
-
+        if(b%5 != 0) {
+            System.out.println(0);
+        }
     }
 }
