aab3b94cd2314dac12929cf5330148e0d3154bbc
tobe1205@hanmail.net <tobe1205@hanmail.net>
2022-03-13 22:10:04 +0900

    [22.03.13] # CodeUp 1355번~1356번 문제 풀이

---
+++ untitled/src/basic4_2_nestedLoop/Q1355/Main.java
@@ -0,0 +1,23 @@
+package basic4_2_nestedLoop.Q1355;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = n; k > i; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ untitled/src/basic4_2_nestedLoop/Q1356/Main.java
@@ -0,0 +1,27 @@
+package basic4_2_nestedLoop.Q1356;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || i == n - 1) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print("*");
+                }
+            } else {
+                System.out.print("*");
+                for (int j = 0; j < n - 2; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
