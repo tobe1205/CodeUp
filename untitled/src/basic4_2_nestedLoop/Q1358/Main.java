32750589b48e38d55fd82da6a20e51f475bdde0a
tobe1205@hanmail.net <tobe1205@hanmail.net>
2022-03-14 12:01:39 +0900

    [22.03.14] # CodeUp 1358번 문제 풀이

--- untitled/src/basic4_2_nestedLoop/Q1358/Main.java
+++ untitled/src/basic4_2_nestedLoop/Q1358/Main.java
@@ -9,15 +9,15 @@ public class Main {
 
         int n = sc.nextInt(); //n 입력 n은 홀수
 
-        for (int i = 0; i <= n; i++) {
-            if (i % 2 != 0) {
-                for (int j = 0; j < i; j++) {
+        for (int i = 0; i <= n; i++) { //줄의 수를 정하는 for문
+            if (i % 2 != 0) { //i의 값이 홀수일 때
+                for (int j = 0; j < i; j++) { //i값 만큼 "*"출력
                     System.out.print("*");
                 }
-                System.out.println();
-            } else {
-                for (int j = i + 2; j <= n; j += 2) {
-                    System.out.print(" ");
+                System.out.println(); //줄바꿈
+            } else { //i의 값이 짝수일 때
+                for (int j = i + 2; j <= n; j += 2) { // i+2부터 길이 n값까지, j값이 2씩 증가할때마다
+                    System.out.print(" ");  //공백 출력
                 }
             }
         }
