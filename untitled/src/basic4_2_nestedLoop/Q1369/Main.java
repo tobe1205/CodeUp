e766732ccb6d4300040209a3152d9fa240f8e125
tobe1205@hanmail.net <tobe1205@hanmail.net>
2022-03-25 10:44:07 +0900

    [22.03.25] # CodeUp 1369번 문제 풀이

--- untitled/src/basic4_2_nestedLoop/Q1369/Main.java
+++ untitled/src/basic4_2_nestedLoop/Q1369/Main.java
@@ -3,6 +3,7 @@ package basic4_2_nestedLoop.Q1369;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
