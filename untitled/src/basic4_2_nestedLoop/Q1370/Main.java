59a8bb76125a363ca01c56d9408f7e0e668415c5
tobe1205@hanmail.net <tobe1205@hanmail.net>
2022-03-31 21:28:34 +0900

    [22.03.31] # CodeUp 1370번 문제 풀이

---
+++ untitled/src/basic4_2_nestedLoop/Q1370/Main.java
@@ -0,0 +1,30 @@
+package basic4_2_nestedLoop.Q1370;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int h = sc.nextInt();
+        int r = sc.nextInt();
+
+        for (int k = 0; k < r; k++) {
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+                System.out.println();
+            }
+            for (int i = h - 2; i >= 0; i--) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+                System.out.println();
+            }
+        }
+    }
+}
+
