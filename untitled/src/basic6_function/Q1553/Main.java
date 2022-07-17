d875d30fc02367ddf6e48c47385ede2f235becec
tobe1205@hanmail.net <tobe1205@hanmail.net>
2022-07-17 11:42:19 +0900

    [22.07.17] # CodeUp 1554번문제 풀이

---
+++ untitled/src/basic6_function/Q1553/Main.java
@@ -0,0 +1,27 @@
+package basic6_function.Q1553;
+
+import java.util.Scanner;
+
+public class Main {
+    static double x;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        x = sc.nextDouble(); //실수형 타입 입력
+
+        System.out.println(f(x));
+
+    }
+
+    private static long f(double x) {
+        long t = (long) x;
+        if (t < x) {
+            return (long) x + 1;
+        } else {
+            return (long) x;
+        }
+    }
+}
+
